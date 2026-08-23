551abe54eba3acb3701a54e2514fb2f89679000a
Alina <virabyanalina2@gmail.com>
2026-08-23 10:56:21 +0400

    Update Merge Sort  algorithm.

---
+++ MergeSort/merge_sort.py
@@ -0,0 +1,40 @@
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+arr_merge = [7, 2, 9, 13, 69, 3]
+sorted_arr_merge = merge_sort(arr_merge)
+
+print("Merge Sort Result:", sorted_arr_merge)
