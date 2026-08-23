4f11c614f06e594b296ceaa1e8c15bcad46801f2
Alina <virabyanalina2@gmail.com>
2026-08-23 10:31:52 +0400

    Organize LinearSearch algorithm

--- LinearSearch.py
+++
@@ -1,9 +0,0 @@
-arr = [-1, 8, 9, 6, 7, 1]
-target = 9
-
-for index in range(len(arr)):
-    if arr[index] == target:
-        print(f"Տարրը գտնվել է ինդեքսում՝ {index}")
-        break
-else:
-    print("Տարրը չի գտնվել զանգվածում")
